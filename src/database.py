680886f11289ba8115b80f27ef1c4ff29f7b231a
framcyber <fram.cyber@gmail.com>
2022-06-05 23:23:17 +0700

    Prepare files for Heroku

--- src/database.py
+++ src/database.py
@@ -1,8 +1,15 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://tanakornwry:1234@localhost/library"
+try:
+    load_dotenv()    
+except:
+    print("Can not read .env file")
+
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://username:password@localhost/library')
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from src import models
 from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
-
+    
 app = FastAPI()
 
 # Dependency
